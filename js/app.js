1850b80812aa7cc73877502213c4977916b8a6e5
Baljeet Jangra <54733764+baljeetjangra@users.noreply.github.com>
2020-02-01 23:42:48 +0530

    secondary banner created

--- js/app.js
+++
@@ -1,9 +0,0 @@
-function OpenTab(tabId) {
-  let tabContent = document.getElementsByClassName("tab-content");
-
-  for (let i = 0; i < tabContent.length; i++) {
-    tabContent[i].style.display = "none";
-  }
-  document.getElementById(tabId).style.display = "block";
-}
-openTab();
